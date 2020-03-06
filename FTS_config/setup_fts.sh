a012dd70aabfc97c50fc203b22eafddff73bc1fd
Gennadiy <lukhanin@fnal.gov>
2020-03-06 05:34:52 -0600

    commit FTS_config area with fts configurations

---
+++ FTS_config/setup_fts.sh
@@ -0,0 +1,18 @@
+#
+# Generic setup script for FTS
+#
+#    You may need to set the explicit version of FTS
+#    $SAM_CP_CONFIG_DIR should point to the directory
+#    where the sam_cp.cfg file resides.
+#
+
+source /home/nfs/icarusraw/ups/setups
+setup ifdhc v2_5_4
+setup sam_cp v9_0_8
+setup encp -q stken:x86_64
+setup ifdhc_config v2_4_5
+
+export SAM_CP_CONFIG_DIR=$PWD
+export X509_USER_PROXY=/opt/icarusraw/icarusraw.Raw.proxy
+
+#export X509_CERT_DIR=
