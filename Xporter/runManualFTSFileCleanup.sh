36c2f1b8b881657e846ad41c59197010cef8c78b
Gennadiy <lukhanin@fnal.gov>
2023-07-25 08:55:03 -0500

    comment on how to run 'online only' version

--- Xporter/runManualFTSFileCleanup.sh
+++ Xporter/runManualFTSFileCleanup.sh
@@ -5,4 +5,7 @@ logfile="/daq/log/fts_logs/`hostname`/FileCleanup_`hostname`_${timestamp}.log"
 
 python3 /home/nfs/icarus/FileTransfer/sbndaq-xporter/Xporter/ManualFTSFileCleanup.py >> ${logfile} 2>&1
 
+#Run with argument 'ONLINE' in order to remove files once they are on dCache, but not confirmed on tape.
+#python3 /home/nfs/icarus/FileTransfer/sbndaq-xporter/Xporter/ManualFTSFileCleanup.py ONLINE >> ${logfile} 2>&1
+
 exit 0
