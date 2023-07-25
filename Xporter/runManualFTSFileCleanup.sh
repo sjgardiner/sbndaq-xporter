34c2f01be525c1a4153a085e9fe42169feac20a7
Gennadiy <lukhanin@fnal.gov>
2023-07-25 08:50:17 -0500

    update to python3

--- Xporter/runManualFTSFileCleanup.sh
+++ Xporter/runManualFTSFileCleanup.sh
@@ -3,6 +3,6 @@
 timestamp=`date +%Y_%m_%d_%H_%M`
 logfile="/daq/log/fts_logs/`hostname`/FileCleanup_`hostname`_${timestamp}.log"
 
-python /home/nfs/icarus/FileTransfer/sbndaq-xporter/Xporter/ManualFTSFileCleanup.py >> ${logfile} 2>&1
+python3 /home/nfs/icarus/FileTransfer/sbndaq-xporter/Xporter/ManualFTSFileCleanup.py >> ${logfile} 2>&1
 
 exit 0
